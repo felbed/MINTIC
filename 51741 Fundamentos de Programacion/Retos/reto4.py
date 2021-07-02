915b6133be0663aeb1f1a8a3a2c2e50c48a4082c
Felipe Bedoya <lufbedoyama@unal.edu.co>
2021-07-01 21:19:01 -0500

    Corrijo: Actualización Reto 4

--- 51741 Fundamentos de Programacion/Retos/reto4.py
+++ 51741 Fundamentos de Programacion/Retos/reto4.py
@@ -12,6 +12,10 @@ print(resultado)
 
 x = input("Ingrese su usuario:\n")
 
+if x == "Tripulante2022":
+    print("Este fue mi primer programa y vamos por más")
+    exit()
+
 if x == user:
     y = input("Ingrese su contraseña:\n")
     if y == password:
@@ -113,62 +117,111 @@ if x == user:
                             break
 
                 elif seleccion == 3:
-                    info = [[2.698, -76.680, 63],
-                            [2.724, -76.693, 20],
-                            [2.606, -76.742, 680],
-                            [2.698, -76.690, 15]]
-
-                    def distancia(latitud, longitud):
-                        R = 6372.795477598
-                        distancias = [[0, 0], [0, 0], [0, 0], [0, 0]]
-                        for i in range(12):
-                            latitud1 = info[i][0]
-                            longitud1 = info[i][1]
-                            usuarios = info[i][2]
-                            delta_lat = latitud1 - latitud
-                            delta_lon = longitud1 - longitud
-                            distancias[i][0] = 2 * R * math.asin(math.sqrt((math.sin(delta_lat/2)**2)+(math.cos(latitud)*math.cos(latitud1))*(math.sin(delta_lon/2)**2)))
-                            distancias[i][1] = usuarios
-                        return sorted(distancias, key = lambda x: x[1])
-
-                    for i in range(4):
-                        latitud = info[i][0]
-                        longitud = info[i][1]
-                        if latitud_inf <= latitud <= latitud_sup and longitud_occ <= longitud <= longitud_or:
-                            continue
-                        else:
-                            print("Error coordenada")
-                            break
-
                     if contador == 0:
                         print("Error sin registro de coordenadas")
+                        break
                     else:
                         for i in range(3):
                             print(f"{i + 1}. Coordenada {sitios[i]}: [Latitud =  {coordenadas[i][0]}, Longitud = {coordenadas[i][1]}]")
 
                         opcion = int(input("Por favor elija su ubicación actual(1,2 ó 3)para calcular ladistancia a los puntos de conexión"))
 
+                        info = [[2.698, -76.680, 63],
+                                [2.724, -76.693, 20],
+                                [2.606, -76.742, 680],
+                                [2.698, -76.690, 15]]
+
+                        for i in range(4):
+                            latitud = info[i][0]
+                            longitud = info[i][1]
+                            if latitud_inf <= latitud <= latitud_sup and longitud_occ <= longitud <= longitud_or:
+                                continue
+                            else:
+                                print("Error coordenada")
+                                break
+
+                        def distancia(latitud, longitud):
+                            R = 6372.795477598
+                            distancias = [[0, 0], [0, 0], [0, 0], [0, 0]]
+                            for i in range(4):
+                                latitud1 = info[i][0]
+                                longitud1 = info[i][1]
+                                usuarios = info[i][2]
+                                delta_lat = latitud1 - latitud
+                                delta_lon = longitud1 - longitud
+                                distancias[i][0] = 2 * R * math.asin(math.sqrt((math.sin(delta_lat/2)**2)+(math.cos(latitud)*math.cos(latitud1))*(math.sin(delta_lon/2)**2)))
+                                distancias[i][1] = usuarios
+                            return sorted(distancias, key = lambda x: x[1])
+
                         if opcion == 1:
                             print("Zonas wifi cercanas con menos usuarios")
                             dist = distancia(latitud=coordenadas[opcion - 1][0], longitud=coordenadas[opcion - 1][1])
                             for i in range(2):
                                 print(f"La zona wifi {i + 1}: ubicada a {dist[i][0]} metros, tiene en promedio {dist[i][1]} usuarios") 
-                            print("Elija 1 o 2 para recibir indicaciones de llegada")
+                            opcion = int(input("Elija 1 o 2 para recibir indicaciones de llegada"))
+                            if opcion == 1:
+                                print("Tiempo en moto")
+                                opcion = int(input("Presione 0 para salir"))
+                                if opcion == 0:
+                                    print("Hasta pronto")
+                                    break
+                            if opcion == 2:
+                                print("Tiempo en bicicleta")
+                                opcion = int(input("Presione 0 para salir"))
+                                if opcion == 0:
+                                    print("Hasta pronto")
+                                    break
+                            else:
+                                print("Error zona wifi")
+                                print("Hasta pronto")
+                                break
                         if opcion == 2:
                             print("Zonas wifi cercanas con menos usuarios")
                             dist = distancia(latitud=coordenadas[opcion - 1][0], longitud=coordenadas[opcion - 1][1])
                             for i in range(2):
-                                print(f"La zona wifi {i + 1}: ubicada a {dist[i][0]} metros, tiene en promedio {dist[i][1]} usuarios") 
-                            print("Elija 1 o 2 para recibir indicaciones de llegada")
+                                print(f"La zona wifi {i + 1}: ubicada a {dist[i][0]} metros, tiene en promedio {dist[i][1]} usuarios")
+                            opcion = int(input("Elija 1 o 2 para recibir indicaciones de llegada"))
+                            if opcion == 1:
+                                print("Tiempo en moto")
+                                opcion = int(input("Presione 0 para salir"))
+                                if opcion == 0:
+                                    print("Hasta pronto")
+                                    break
+                            if opcion == 2:
+                                print("Tiempo en bicicleta")
+                                opcion = int(input("Presione 0 para salir"))
+                                if opcion == 0:
+                                    print("Hasta pronto")
+                                    break
+                            else:
+                                print("Error zona wifi")
+                                print("Hasta pronto")
+                                break
                         if opcion == 3:
                             print("Zonas wifi cercanas con menos usuarios")
                             dist = distancia(latitud=coordenadas[opcion - 1][0], longitud=coordenadas[opcion - 1][1])
                             for i in range(2):
-                                print(f"La zona wifi {i + 1}: ubicada a {dist[i][0]} metros, tiene en promedio {dist[i][1]} usuarios") 
-                            print("Elija 1 o 2 para recibir indicaciones de llegada")
+                                print(f"La zona wifi {i + 1}: ubicada a {dist[i][0]} metros, tiene en promedio {dist[i][1]} usuarios")
+                            opcion = int(input("Elija 1 o 2 para recibir indicaciones de llegada"))
+                            if opcion == 1:
+                                print("Tiempo en moto")
+                                opcion = int(input("Presione 0 para salir"))
+                                if opcion == 0:
+                                    print("Hasta pronto")
+                                    break
+                            if opcion == 2:
+                                print("Tiempo en bicicleta")
+                                opcion = int(input("Presione 0 para salir"))
+                                if opcion == 0:
+                                    print("Hasta pronto")
+                                    break
+                            else:
+                                print("Error zona wifi")
+                                print("Hasta pronto")
+                                break
                         else:
                             print("Error ubicación")
-                            condicion = False
+                            condicion = False             
                 elif seleccion == 4:
                     condicion = False
                 elif seleccion == 5:
@@ -216,6 +269,14 @@ if x == user:
                             print("Error")
                             print("Hasta pronto")
                             condicion = False
+                if seleccion == 2021:
+                    lat = float(input("Dame una latitudy te diré cual hemisferio es..."))
+                    if lat > 0:
+                        print("Usted está en hemisferio norte")
+                        break
+                    if lat < 0:
+                        print("Usted está en hemisferio sur")
+                        break
                 elif seleccion == 7:
                     print("Hasta pronto")
                     exit()
