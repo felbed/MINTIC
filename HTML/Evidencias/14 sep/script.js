16d343442348610d618e869f1fdd64e7b2d622d0
Felipe Bedoya <lufbedoyama@unal.edu.co>
2021-09-14 21:43:37 -0500

    Actualización de la página web
    Se intentan crear ventanas independiente después de oprimir el botón.

---
+++ HTML/Evidencias/14 sep/script.js
@@ -0,0 +1,8 @@
+function ShowAndHide() {
+    var x = document.getElementById('no-background');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
